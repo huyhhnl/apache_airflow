2ff019c0e774c43aec34127a2e41f9d707d8ca81
huyhhnl <110181079+huyhhnl@users.noreply.github.com>
2023-09-26 17:04:09 +0700

    Create config.py

---
+++ airflow_spark_mongo_project/config.py
@@ -0,0 +1,10 @@
+QUESTION_FILE_PATH = '/home/airflow/airflow/Questions.csv'
+ANSWER_FILE_PATH = '/home/airflow/airflow/Answers.csv'
+SPARK_OUTPUT_PATH = '/home/airflow/airflow/Spark_Output'
+SPARK_PYTHON_FILE_PATH = '/home/airflow/airflow/dags/spark_code/count_answer.py'
+
+ANSWER_DRIVER_ID = '1gEU9KojKI3yMybjL0_tYsQnzRlUoBYro'
+QUESTION_DRIVER_ID ='1yH2lTY3mGOoGEDW1Y2lhzRwDuaZvI1KI'
+
+# https://drive.google.com/file/d/1gEU9KojKI3yMybjL0_tYsQnzRlUoBYro/view?usp=sharing
+# https://drive.google.com/file/d/1yH2lTY3mGOoGEDW1Y2lhzRwDuaZvI1KI/view?usp=sharing
